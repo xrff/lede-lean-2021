35f6b05ccecc1e2438dc7c84ad31ba57b96e1673
xrff <15010254640@qq.com>
2021-09-17 18:13:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_generate
+
+## Modify default hostname
+sed -i 's/OpenWrt/XRFF/g' package/base-files/files/bin/config_generate
